b3d206dec3abd5c27e6dfa3f3aa1e7b524e2a52c
rcunico <rcunico@ggc.edu>
2019-09-08 22:19:55 -0400

    Updates

---
+++ precache-manifest.c3daf2b48c19aa146111ec78dd77ed52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e872ec84aea8f3f3716c814eb577c5",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "79adef4dfcac61438c93",
+    "url": "/react_todo/static/js/2.e1de7348.chunk.js"
+  },
+  {
+    "revision": "041950a1557a1aed1921",
+    "url": "/react_todo/static/js/main.63204d7a.chunk.js"
+  },
+  {
+    "revision": "8b42469b5cdc11b5544a",
+    "url": "/react_todo/static/js/runtime~main.a93a1dc3.js"
+  }
+]);
